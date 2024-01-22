95d99976cd5c1634de950897cbf6b96dc74b0c50
Terry Tompkins <me@terrytompkins.com>
2024-01-22 00:08:55 -0500

    updated to new django full model generator

--- retail_store/models.py
+++ retail_store/models.py
@@ -1,22 +1,23 @@
 from django.db import models
-
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    published_date = models.DateField()
-
-    def __str__(self):
-        return self.title
-
-
-
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-    birth_date = models.DateField()
-
-    def __str__(self):
-        return self.name
-
+class MyModel(models.Model):
+	id = models.AutoField(primary_key=true)
+	name = models.CharField(max_length=100, unique=true, blank=false, null=false)
+	created_at = models.DateTimeField(auto_now_add=true)
+	updated_at = models.DateTimeField(auto_now=true)
+	related_model = models.ForeignKey(to='RelatedModel', related_name='my_model', on_delete=models.CASCADE)
+    
+	class Meta:
+		ordering = -created_at
+		verbose_name = 'My Model'
+	
+		def __str__(self):
+			return self.id
+class RelatedModel(models.Model):
+	id = models.AutoField(primary_key=true)
+	info = models.TextField(blank=true)
+	
+	class Meta:
+	
+		def __str__(self):
+			return self.id
 
